bf67a38590acdc70a92769de23f7a662105c837e
raph bmw <theuitheuil41150@gmail.com>
2025-06-11 18:22:23 +0700

    logo reseaux

--- src/components/layout/SocialButtons.tsx
+++ src/components/layout/SocialButtons.tsx
@@ -1,39 +1,68 @@
 import React from "react";
 import "./SocialButtons.css";
 
+const buttons = [
+  {
+    className: "github",
+    href: "https://github.com/Raphael91000",
+    label: "GitHub",
+    svg: (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+        <path
+          d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+        />
+      </svg>
+    ),
+  },
+  {
+    className: "linkedin",
+    href: "https://www.linkedin.com/in/raphael-theuillon-689139261/",
+    label: "LinkedIn",
+    svg: (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path
+          d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+        />
+      </svg>
+    ),
+  },
+  {
+    className: "fiverr",
+    href: "https://www.fiverr.com/sellers/raph910/edit",
+    label: "Fiverr",
+    svg: (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+        <circle cx="200" cy="200" r="180" fill="currentColor" stroke="white" strokeWidth="8"/>
+        <text x="200" y="240" textAnchor="middle" fontSize="200" fontWeight="bold" fill="white">F</text>
+      </svg>
+    ),
+  },
+];
+
 const SocialButtons: React.FC = () => {
   return (
-    <div className="card">
-      <div className="background"></div>
-      <div className="logo">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.667 31.69" className="logo-svg">
-          <path id="Path_6" data-name="Path 6" d="M12.827,1.628A1.561,1.561,0,0,1,14.31,0h2.964a1.561,1.561,0,0,1,1.483,1.628v11.9a9.252,9.252,0,0,1-2.432,6.852q-2.432,2.409-6.963,2.409T2.4,20.452Q0,18.094,0,13.669V1.628A1.561,1.561,0,0,1,1.483,0h2.98A1.561,1.561,0,0,1,5.947,1.628V13.191a5.635,5.635,0,0,0,.85,3.451,3.153,3.153,0,0,0,2.632,1.094,3.032,3.032,0,0,0,2.582-1.076,5.836,5.836,0,0,0,.816-3.486Z" transform="translate(0 0)"></path>
-          <path id="Path_7" data-name="Path 7" d="M75.207,20.857a1.561,1.561,0,0,1-1.483,1.628h-2.98a1.561,1.561,0,0,1-1.483-1.628V1.628A1.561,1.561,0,0,1,70.743,0h2.98a1.561,1.561,0,0,1,1.483,1.628Z" transform="translate(-45.91 0)"></path>
-          <path id="Path_8" data-name="Path 8" d="M0,80.018A1.561,1.561,0,0,1,1.483,78.39h26.7a1.561,1.561,0,0,1,1.483,1.628v2.006a1.561,1.561,0,0,1-1.483,1.628H1.483A1.561,1.561,0,0,1,0,82.025Z" transform="translate(0 -51.963)"></path>
+    <div className="social-clock">
+      <div className="social-clock__list">
+        {buttons.map((btn, i) => (
+          <a
+            key={i}
+            href={btn.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`social-clock__button ${btn.className}`}
+            aria-label={btn.label}
+          >
+            {btn.svg}
+          </a>
+        ))}
+      </div>
+      <button className="social-clock__trigger">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path
+            d="M352 224c53 0 96-43 96-96s-43-96-96-96s-96 43-96 96c0 4 .2 8 .7 11.9l-94.1 47C145.4 170.2 121.9 160 96 160c-53 0-96 43-96 96s43 96 96 96c25.9 0 49.4-10.2 66.6-26.9l94.1 47c-.5 3.9-.7 7.8-.7 11.9c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-25.9 0-49.4 10.2-66.6 26.9l-94.1-47c.5-3.9 .7-7.8 .7-11.9s-.2-8-.7-11.9l94.1-47C302.6 213.8 326.1 224 352 224z"
+          />
         </svg>
-      </div>
-      <div className="box box1">
-        <a href="https://github.com/Raphael91000" target="_blank" rel="noopener noreferrer" className="icon">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="svg">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
-          </svg>
-        </a>
-      </div>
-      <div className="box box2">
-        <a href="https://www.linkedin.com/in/raphael-theuillon-689139261/" target="_blank" rel="noopener noreferrer" className="icon">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="svg">
-            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-1.637-.557-2.756-1.954-2.756-1.065 0-1.698.715-1.698 1.617 0 .875.583 1.604 1.433 1.604h.032l-.001 5.139h-3v-11h2.987v1.437h.043c.416-.787 1.428-1.617 2.933-1.617 3.141 0 3.711 2.066 3.711 4.751v5.429z"/>
-          </svg>
-        </a>
-      </div>
-      <div className="box box3">
-        <a href="https://www.fiverr.com/sellers/raph910/edit" target="_blank" rel="noopener noreferrer" className="icon">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="svg">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
-          </svg>
-        </a>
-      </div>
-      <div className="box box4"></div>
+      </button>
     </div>
   );
 };
